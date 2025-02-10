c2bba8294ce56405f064e208c60da1878bdff720
MiSa-JD <jeongs0427@gmail.com>
2025-02-10 19:50:27 +0900

    add .files

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  bracetSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  semi: false,
+  printWidth: 90,
+  tabWidth: 2
+}
